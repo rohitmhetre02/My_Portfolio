f4a0d53a8db7c50917cc84bac449ccdcd0fd5300
Rohit mhetre <rohitmhetre2004@gmail.com>
2025-02-26 23:00:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const PORT = process.env.PORT || 5000; 
 
 const app = express();
 app.use(cors());
@@ -39,4 +40,6 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+  console.log(`Server Started ont ${PORT}`);
+});
